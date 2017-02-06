7170ab9d248cdd43317a918cec3abdbf0698b5a7
Diego Canton de Brito <diegocantonbgi@gmail.com>
2017-02-06 09:24:03 -0200

    Update spfblstats.sh

--- spfblstats.sh
+++ spfblstats.sh
@@ -49,7 +49,7 @@ case $1 in
 
 	TOTALES=$(echo $WHITE + $BLOCKED + $FLAG + $HOLD + $NXDOMAIN + $NXSENDER + $PASS + $TIMEOUT + $NONE + $SOFTFAIL + $NEUTRAL + $INTERRUPTED + $SPAMTRAP + $INEXISTENT + $INVALID + $FAIL | bc)
 	BLOCKTOTAL=$( echo $BLOCKED + $FLAG + $HOLD + $NXDOMAIN + $NXSENDER + $TIMEOUT + $NONE + $SOFTFAIL + $NEUTRAL + $INTERRUPTED + $SPAMTRAP + $INEXISTENT + $INVALID + $FAIL | bc)
-	PASSTOTAL=$( $WHITE + $PASS | bc)
+	PASSTOTAL=$( echo $WHITE + $PASS | bc)
 
 	GREYLIST=$(grep -c GREYLIST "$LOGTEMP")
 	LISTED=$(grep -c LISTED "$LOGTEMP")
@@ -80,7 +80,7 @@ case $1 in
 	echo 'INEXISTENT:' $(echo "scale=0;($INEXISTENT*100) / $TOTALES" | bc)'% - '"$INEXISTENT"
 	echo '   TIMEOUT:' $(echo "scale=0;($TIMEOUT*100) / $TOTALES" | bc)'% - '"$TIMEOUT"
 	echo '  ----------------------'
-	echo '   ALL BLOCKED:' $(echo "scale=0;($BLOCKTOTAL*100) / $TOTALES" | bc)'% - '"$BLOCKTOTAL"
+	echo '   ALL BLOCKED: ' $(echo "scale=0;($BLOCKTOTAL*100) / $TOTALES" | bc)'% - '"$BLOCKTOTAL"
 	echo '   ALL ACCEPTED:' $(echo "scale=0;($PASSTOTAL*100) / $TOTALES" | bc)'% - '"$PASSTOTAL"
 	echo '     TOTAL:' $(echo "scale=0;($TOTALES*100) / $TOTALES" | bc)'% - '"$TOTALES"
 	echo '=========================='
